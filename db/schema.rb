faa531e98b63b757fd37d8936d7373d3ee986a10
Ryna Truong <rynatruong@outlook.com>
2020-09-27 21:40:33 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20200925124033) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+  end
 
 end
